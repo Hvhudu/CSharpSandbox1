c6a5db87759b879f90531a515bb7e357eb3176f6
Hvhudu <shtormbreker2@gmail.com>
2021-11-09 22:38:00 +0300

    Второе задание

---
+++ Ex2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ex2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите длинну окружности: ");
+            double leight = Convert.ToDouble(Console.ReadLine());
+            const double PI = 3.14;
+            double result = leight / (2 * PI);
+            Console.WriteLine($"Радиус равен: {result}");
+
+        }
+    }
+
+
+}
