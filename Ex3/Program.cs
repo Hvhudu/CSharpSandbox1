810695f29ee41ea1b10a75212b2ff7cb3e1095a5
Hvhudu <shtormbreker2@gmail.com>
2021-11-10 22:13:31 +0300

    Третье задание

---
+++ Ex3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ex3
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            int x = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 3; i >= 0; i--)
+            {
+                sum += x / (int)Math.Pow(10.0, i);
+                x = x % (int)Math.Pow(10.0, i);
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
