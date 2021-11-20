07eadc194cf11148b1acf7564abdafa156109b33
Hvhudu <shtormbreker2@gmail.com>
2021-11-20 12:40:31 +0300

    Разложение на простые множители

---
+++ Homework3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.Write("Введите число : ");
+             int num = Convert.ToInt32(Console.ReadLine());
+             Console.Write("{0} = 1", num);
+             for (int i = 0; num % 2 == 0; num /= 2)
+             {
+                    Console.Write(" * {0}", 2);
+             }
+             for (int i = 3; i <= num;)
+             {
+                if (num % i == 0)
+                {
+                    Console.WriteLine(" * {0}", i);
+                    num /= i;
+                }
+                else
+                {
+                    i += 2;
+                }
+             }         
+        }
+    }
+}
+    
